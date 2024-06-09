d4ee7f89a3ab77610814d96fb0432e395ea0d5b1
samiha26 <samiha.tasnim143@gmail.com>
2024-06-09 16:52:00 +0800

    commit

--- dfsdb/model_loaders/person_loader.py
+++ dfsdb/model_loaders/person_loader.py
@@ -1,11 +1,14 @@
 import os
-
 import numpy as np
 import torch
 from PIL import Image
 from torch import nn
 from torchvision import models, transforms
+import logging
 
+
+#setting up log file
+logging.basicConfig(filename='error.log', level=logging.ERROR, format='%(asctime)s - %(levelname)s: %(message)s')
 
 class MultilabelClassifier(nn.Module):
     """
@@ -14,21 +17,29 @@ class MultilabelClassifier(nn.Module):
     """
     def __init__(self, n_features):
         super().__init__()
-        self.resnet = models.resnet34(pretrained=True)
-        self.model_wo_fc = nn.Sequential(*(list(self.resnet.children())[:-1]))
+        try:
+            self.resnet = models.resnet34(pretrained=True)
+            self.model_wo_fc = nn.Sequential(*(list(self.resnet.children())[:-1]))
 
-        self.imageClass = nn.Sequential(
-            nn.Dropout(p=0.2),
-            nn.Linear(in_features=512, out_features=n_features)
-        )
+            self.imageClass = nn.Sequential(
+                nn.Dropout(p=0.2),
+                nn.Linear(in_features=512, out_features=n_features)
+            )
+        except Exception as e:
+            logging.error("Error initializing MultilabelClassifier: %s", e)
+            raise
 
     def forward(self, x):
-        x = self.model_wo_fc(x)
-        x = torch.flatten(x, 1)
+        try:
+            x = self.model_wo_fc(x)
+            x = torch.flatten(x, 1)
 
-        return {
-            'class': self.imageClass(x)
-        }
+            return {
+                'class': self.imageClass(x)
+            }
+        except Exception as e:
+            logging.error("Error in forward pass function of MultilabelClassifier: %s", e)
+            raise
 
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
@@ -46,24 +57,28 @@ def predict(modelPath, imagePath):
     :param imagePath: The path to the image to be classified
     :return: The label of the image as predicted by the model
     """
-    model = MultilabelClassifier(3).to(device)
-    optimizer = torch.optim.Adam(model.parameters(), lr=0.0001)
-    checkpoint = torch.load(modelPath)
-    model.load_state_dict(checkpoint['model_state_dict'])
-    optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
-    loss = checkpoint['loss']
-    model.eval()
+    try:
+        model = MultilabelClassifier(3).to(device)
+        optimizer = torch.optim.Adam(model.parameters(), lr=0.0001)
+        checkpoint = torch.load(modelPath)
+        model.load_state_dict(checkpoint['model_state_dict'])
+        optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+        loss = checkpoint['loss']
+        model.eval()
 
-    img_loc = imagePath
-    raw_img = Image.open(img_loc).convert('RGB')
-    single_img = data_transforms(raw_img)
-    single_img = single_img.unsqueeze(0)
-    tmp = torch.from_numpy(np.array(single_img))
-    outputs = model(tmp.to(device))
-    res = 0
-    for i, out in enumerate(outputs):
-        _, predicted = torch.max(outputs[out], 1)
-        res = predicted.item()
+        img_loc = imagePath
+        raw_img = Image.open(img_loc).convert('RGB')
+        single_img = data_transforms(raw_img)
+        single_img = single_img.unsqueeze(0)
+        tmp = torch.from_numpy(np.array(single_img))
+        outputs = model(tmp.to(device))
+        res = 0
+        for i, out in enumerate(outputs):
+            _, predicted = torch.max(outputs[out], 1)
+            res = predicted.item()
 
-    return res
+        return res
+    except Exception as e:
+        logging.error("Error in predict function: %s", e)
+        raise
 
